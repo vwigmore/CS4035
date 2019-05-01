4099c82b8973a5f321c076936ff82aa70dd043b1
Viktor <vwigore@student.tudelft.nl>
2019-05-01 14:31:44 +0200

    hmap

--- heatmap.py
+++ heatmap.py
@@ -1,5 +1,8 @@
+# coding: utf-8
+
 import datetime
 import time
+import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn import neighbors
 from sklearn.metrics import precision_recall_curve
@@ -10,10 +13,99 @@ from operator import itemgetter
 from itertools import groupby
 import numpy as np
 
+def string_to_timestamp(date_string):#convert time string to float value
+    time_stamp = time.strptime(date_string, '%Y-%m-%d %H:%M:%S')
+    return time.mktime(time_stamp)
 
 if __name__ == "__main__":
 
     src = 'data_for_student_case.csv'
     ah = open(src, 'r')
 
-    
+    x = []  # contains features
+    y = []  # contains labels
+    data = []
+    color = []
+    (issuercountry_set, txvariantcode_set, currencycode_set, shoppercountry_set, interaction_set,
+     verification_set, accountcode_set, mail_id_set, ip_id_set, card_id_set) = [set() for _ in xrange(10)]
+    (issuercountry_dict, txvariantcode_dict, currencycode_dict, shoppercountry_dict, interaction_dict,
+     verification_dict, accountcode_dict, mail_id_dict, ip_id_dict, card_id_dict) = [{} for _ in xrange(10)]
+    # label_set
+    # cvcresponse_set = set()
+    ah.readline()  # skip first line
+    count = 0
+    for line_ah in ah:
+        if line_ah.strip().split(',')[9]=='Refused':# remove the row with 'refused' label, since it's uncertain about fraud
+            continue
+        if 'na' in str(line_ah.strip().split(',')[14]).lower() or 'na' in str(line_ah.strip().split(',')[4].lower()):
+            continue
+        bookingdate = string_to_timestamp(line_ah.strip().split(',')[1])# date reported flaud
+        issuercountry = line_ah.strip().split(',')[2]#country code
+        issuercountry_set.add(issuercountry)
+        txvariantcode = line_ah.strip().split(',')[3]#type of card: visa/master
+        txvariantcode_set.add(txvariantcode)
+        issuer_id = float(line_ah.strip().split(',')[4])#bin card issuer identifier
+        amount = float(line_ah.strip().split(',')[5])#transaction amount in minor units
+        currencycode = line_ah.strip().split(',')[6]
+        currencycode_set.add(currencycode)
+        shoppercountry = line_ah.strip().split(',')[7]#country code
+        shoppercountry_set.add(shoppercountry)
+        interaction = line_ah.strip().split(',')[8]#online transaction or subscription
+        interaction_set.add(interaction)
+        if line_ah.strip().split(',')[9] == 'Chargeback':
+            label = 1#label fraud
+            count += 1
+        else:
+            label = 0#label save
+        verification = line_ah.strip().split(',')[10]#shopper provide CVC code or not
+        verification_set.add(verification)
+        cvcresponse = line_ah.strip().split(',')[11]#0 = Unknown, 1=Match, 2=No Match, 3-6=Not checked
+        if cvcresponse > 2:
+            cvcresponse = 3
+        year_info = datetime.datetime.strptime(line_ah.strip().split(',')[12],'%Y-%m-%d %H:%M:%S').year
+        month_info = datetime.datetime.strptime(line_ah.strip().split(',')[12],'%Y-%m-%d %H:%M:%S').month
+        day_info = datetime.datetime.strptime(line_ah.strip().split(',')[12],'%Y-%m-%d %H:%M:%S').day
+        creationdate = str(year_info)+'-'+str(month_info)+'-'+str(day_info)#Date of transaction
+        creationdate_stamp = string_to_timestamp(line_ah.strip().split(',')[12])#Date of transaction-time stamp
+        accountcode = line_ah.strip().split(',')[13]#merchant’s webshop
+        accountcode_set.add(accountcode)
+        mail_id = int(float(line_ah.strip().split(',')[14].replace('email','')))#mail
+        mail_id_set.add(mail_id)
+        ip_id = int(float(line_ah.strip().split(',')[15].replace('ip','')))#ip
+        ip_id_set.add(ip_id)
+        card_id = int(float(line_ah.strip().split(',')[16].replace('card','')))#card
+        card_id_set.add(card_id)
+        data.append([issuercountry, txvariantcode, issuer_id, amount, currencycode,
+                    shoppercountry, interaction, verification, cvcresponse, creationdate_stamp,
+                     accountcode, mail_id, ip_id, card_id, label, creationdate])# add the interested features here
+
+    print(count)
+    par1 = txvariantcode_set
+    par2 = currencycode_set
+
+    heat_map = np.zeros((len(par1), len(par2)))
+    plist1 = list(par1)
+    plist2 = list(par2)
+
+    for [issuercountry, txvariantcode, issuer_id, amount, currencycode,
+                    shoppercountry, interaction, verification, cvcresponse, creationdate_stamp,
+                     accountcode, mail_id, ip_id, card_id, label, creationdate] in data:
+        if label == 1:
+            i = plist1.index(txvariantcode)
+            j = plist2.index(currencycode)
+            heat_map[i][j] = heat_map[i][j] + 1
+
+    print(heat_map.sum())
+    sns.heatmap(heat_map, xticklabels=plist2, yticklabels=plist1)
+    plt.show()
+
+
+
+
+
+
+
+
+
+
+
